7fa31c57f492bfb7275f823498786868294bc2fc
TurtleWhal <garrettrjordan@gmail.com>
2025-02-27 15:07:36 -0800

    added autos stuff

---
+++ src/main/java/frc/robot/Autos.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Autos extends SubsystemBase {
+    public Autos() {
+        // This is where you can add autonomous routines
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
